e5f5bf05d9b859ba39871dbd62d68315f178ae5c
X <55091572+yulinzhouZYL@users.noreply.github.com>
2025-05-15 12:09:56 +0800

    Update evaluate_model_insert.py

--- test/evaluate_model_insert.py
+++ test/evaluate_model_insert.py
@@ -12,18 +12,6 @@ from M_dataset import MambaSequenceDataset
 from torch.utils.data import DataLoader
 import time
 from tqdm import tqdm
-#
-# def sample_box_pose():
-#     x_range = [0.0, 0.2]
-#     y_range = [0.4, 0.6]
-#     z_range = [0.05, 0.05]
-#
-#     ranges = np.vstack([x_range, y_range, z_range])
-#     cube_position = np.random.uniform(ranges[:, 0], ranges[:, 1])
-#
-#     cube_quat = np.array([1, 0, 0, 0])
-#     return np.concatenate([cube_position, cube_quat])
-
 
 def sample_insertion_pose():
     # Peg
@@ -126,7 +114,6 @@ print("reset hiddens finish")
 print("warmup model finish")
 for rollout_id in range(num_rollouts):
     rollout_id += 0
-    # BOX_POSE[0] = sample_insertion_pose()
     # 增加FIFO加权动作队列
     all_time_actions = torch.zeros([max_timesteps, max_timesteps + num_queries, state_dim]).cuda()
     BOX_POSE[0] = np.concatenate(sample_insertion_pose())  # 生成14维数组随机初始位置
